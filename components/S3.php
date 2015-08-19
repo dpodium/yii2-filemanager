beb46dc2539d1d21ad6b5537df5b112127acaeca
ruijun <june.see@dpodium.com>
2015-08-19 18:07:44 +0800

    Set aws s3 cache control.

--- components/S3.php
+++ components/S3.php
@@ -63,6 +63,7 @@ class S3 {
                 'SourceFile' => $file->tempName,
                 'ContentType' => $file->type,
                 'ACL' => 'public-read',
+                'CacheControl' => '2592000' // 30 days
             ]);
 
             $result['status'] = true;
