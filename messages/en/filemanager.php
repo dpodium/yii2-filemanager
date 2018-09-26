dc96ed777d9b52245a1e38d85555adad42d9a8f8
Dampfklon <me@dampfklon.de>
2018-09-26 11:20:48 +0200

    yii\web\UploadedFile Error Handling (#25)
    Added missing Error handling for yii\web\UploadedFile

--- messages/en/filemanager.php
+++ messages/en/filemanager.php
@@ -41,6 +41,7 @@ return [
     'File Name' => '',
     'File dimension at most 2272 X 1704.' => '',
     'File not found.' => '',
+    'File too large.' => '',
     'Filename can only contain alphanumeric characters, underscores and dashes.' => '',
     'Folder ID' => '',
     'Invalid file extension. Available file extension are {ext}' => '',
